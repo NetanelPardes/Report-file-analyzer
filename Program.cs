7faeb4dc78612dcc1a14d7050feae2963ca31fb5
Netanel Pardes <sendi8475@gmail.com>
2026-06-24 12:38:50 +0300

    add a function that chack the line

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 
 namespace program;
@@ -9,7 +10,8 @@ class program
     {
         if (!File.Exists(myPath))
         {
-            Console.WriteLine("Error: File 'reports.txt' not found");
+            string[] my_file = myPath.Split('\\');
+            Console.WriteLine($"Error: File {my_file[my_file.Length-1]} not found");
             return [];
         }
         string[] myFileString = File.ReadAllLines(myPath);
@@ -24,6 +26,43 @@ class program
         }
         return myResult;
     }
+    static string[] SplitLine(string line)
+    {
+        string[] myLine = line.Split(',', StringSplitOptions.TrimEntries);
+
+        return myLine;
+    }
+    static bool CountLine(String[] myLine)
+    {
+        if(myLine.Length == 5)
+        {
+            return true;
+        }
+        return false;
+    }
+    static bool Verification(String[] myLine)
+    {
+        int priority;
+        if(!int.TryParse(myLine[2],out priority) || (priority < 1 && priority > 5))
+        {
+            //need to fix
+            Console.WriteLine("reason:Invalid record");
+            return false;
+        }
+        double score;
+        if(!double.TryParse(myLine[3], out score) || (score < 0.0 && score > 100.0))
+        {
+            //need to fix
+            Console.WriteLine("reason:Invalid record");
+            return false;
+        }
+        Console.WriteLine("Valid record processed");
+        return true;
+    }
+    //static int ProcessReports(List<string> myRepo)
+    //{
+
+    //}
     static void Main()
     {
         string path = @"..\..\..\reports.txt";
@@ -32,7 +71,10 @@ class program
         //{
         //    Console.WriteLine(t);
         //}
-        
-
+        if (myRepors.Count > 0)
+        {
+            Console.WriteLine($"File loaded: {myRepors.Count} lines found");
+        }
+        //ProcessLine("Alpha, Collect,3.87,5,Approved");
     }
 }
