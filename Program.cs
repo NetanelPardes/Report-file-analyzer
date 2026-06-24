0b210ec772e7bb84406604ae71977da0222f1405
Netanel Pardes <sendi8475@gmail.com>
2026-06-24 11:14:29 +0300

    add a function that load data from a text file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace program;
+
+class program
+{
+    static List<string> LoadFile(string myPath)
+    {
+        if (!File.Exists(myPath))
+        {
+            Console.WriteLine("Error: File 'reports.txt' not found");
+            return [];
+        }
+        string[] myFileString = File.ReadAllLines(myPath);
+        List<string> myResult = new List<string>();
+        foreach(string report in myFileString)
+        {
+            myResult.Add(report);
+        }
+        if (myFileString.Length == 0)
+        {
+            Console.WriteLine("Error: File is empty");
+        }
+        return myResult;
+    }
+    static void Main()
+    {
+        string path = @"..\..\..\reports.txt";
+        List<string> myRepors = LoadFile(path);
+        //foreach(string t in myRepors)
+        //{
+        //    Console.WriteLine(t);
+        //}
+        
+
+    }
+}
